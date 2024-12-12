8f5c5dcbecc1b9b405f8144cb9734fbde2eef0d8
krishnayoga <krishna603@kookmin.ac.kr>
2024-12-12 20:45:57 +0900

    add loss function

---
+++ utils/saveModel.py
@@ -0,0 +1,24 @@
+import torch
+
+def saveModel(model, optimizer, lr_scheduler, epoch, loss, path):
+    """
+    Save the model to the disk
+
+    Args:
+    - model: The model to save
+    - optimizer: The optimizer used to train the model
+    - lr_scheduler: The learning rate scheduler used to train the model
+    - epoch: The current epoch
+    - loss: The loss at the current epoch
+
+    Returns:    
+    - None
+    """
+
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'lr_scheduler_state_dict': lr_scheduler.state_dict(),
+        'loss': loss,
+    }, path)
