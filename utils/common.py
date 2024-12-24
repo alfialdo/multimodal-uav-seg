90556cf81a830be83429253a5a1827f97485c631
Alfi Aldo <aldoalfi02@gmail.com>
2024-12-24 18:59:58 +0900

    update: added IoU Loss function

--- utils/common.py
+++ utils/common.py
@@ -12,6 +12,8 @@ def get_loss_function(loss_fn):
         return torch.nn.BCEWithLogitsLoss()
     elif loss_fn == 'DiceLoss':
         return smp.losses.DiceLoss(mode='binary')
+    elif loss_fn == 'IoULoss':
+        return smp.losses.JaccardLoss(mode='binary')
     else:
         raise ValueError(f'Loss function {loss_fn} not supported')
     
